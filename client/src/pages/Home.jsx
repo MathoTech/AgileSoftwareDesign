9e28c63f97cceaf5ff85a120490f60ef04e14b9f
Thomas Lopez <thomas1.lopez@epitech.eu>
2022-11-09 18:55:03 +0100

    [ADD] role button + animations

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import './Home.css';
-import NavigationBar from './components/navigationBar';
+import NavigationBar from '../components/navigationBar';
 import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Home = () => {
   const [firstName, setFirstName] = useState("")
@@ -10,7 +12,8 @@ const Home = () => {
   const [citizenship, setCitizenship] = useState("")
   const [nationality, setNationality] = useState("")
   const [codiceFiscal, setCodiceFiscal] = useState("")
-  
+  const [role, setRole] = useState("")
+
 
   const handleFirstNameChange = (event) => {
     setFirstName(event.target.value)
@@ -36,11 +39,19 @@ const Home = () => {
   const handleCodiceFiscalChange = (event) => {
     setCodiceFiscal(event.target.value)
   }
+  const handleRoleChange = (event) => {
+    setRole(event.target.value)
+  }
 
   const handleSubmission = (event) => {
     const data = new FormData(event.target);
     console.log(data);
     //envoyer requete Post a un serveur
+  }
+
+  function addStyle() {
+    var element = document.getElementById("cards");
+    element.classList.add("visible");
   }
   return (
     <div className="App">
@@ -53,7 +64,39 @@ const Home = () => {
           <div className="title-white">Attempt to</div>
           <div className="title-blue"> Universita della Calabria</div>
         </div>
-        <div className="card-container">
+        <div className="roles">
+          <div className="role">
+            <div className="role-header">
+              ERASMUS
+            </div>
+            <hr className="line"/>
+            <div className="role-content">
+              <a className="role-button" onClick={addStyle}>Subscribe</a>
+            </div>
+          </div>
+          <div className="role">
+            <div className="role-header">
+              FOREIGNERS
+            </div>
+            <hr className="line"/>
+            <div className="role-content">
+              <a className="role-button" onClick={addStyle}>Subscribe</a>
+            </div>
+
+          </div>
+          <div className="role">
+            <div className="role-header">
+              ITALIAN
+            </div>
+            <hr className="line"/>
+            <div className="role-content" onClick={addStyle}>
+              <a className="role-button">Subscribe</a>
+            </div>
+
+          </div>
+
+        </div>
+         <div className="card-container" id="cards">
           <div className="card">
             <div className="card-header">
               PERSONAL DATA
@@ -86,54 +129,45 @@ const Home = () => {
               </div>
               <div className="field-container">
                 <div className="field-text">Date of birth:</div>
-                <input
-                  className="field"
-                  id="outlined-basic"
-                  variant="outlined"
-                  fullWidth
-                  label="Search"
-                  onChange={handleBirthChange}
-                  value={birth}
-                  placeholder="dd/mm/yyyy"
-                />
+                <DatePicker className="field" selected={birth} onChange={(date) => setBirth(date)} />
               </div>
               <div className="field-container">
                 <div className="field-text">Gender:</div>
                 <fieldset className="set">
                   <input name="gender" type="radio" id="male" value="male" onChange={handleGenderChange}
-                  value={gender}/>
-                    <img className="gender-icon" src="/icons/male.png"/>
-                    <input name="gender" type="radio" id="female" value="female" onChange={handleGenderChange}
-                  value={gender}/>
-                    <img className="gender-icon" src="/icons/female.png"/>
+                    value={gender} />
+                  <img className="gender-icon" src="/icons/male.png" />
+                  <input name="gender" type="radio" id="female" value="female" onChange={handleGenderChange}
+                    value={gender} />
+                  <img className="gender-icon" src="/icons/female.png" />
                 </fieldset>
               </div>
               <div className="field-container">
                 <div className="field-text">Citizenship:</div>
                 <select id="citizenship">
-                <option value="france">France</option>
-                <option value="italy">Italy</option>
+                  <option value="france">France</option>
+                  <option value="italy">Italy</option>
                 </select>
               </div>
               <div className="field-container">
                 <div className="field-text">Nationality:</div>
                 <select id="citizenship">
-                <option value="france">France</option>
-                <option value="italy">Italy</option>
+                  <option value="france">France</option>
+                  <option value="italy">Italy</option>
                 </select>
               </div>
               <div className="field-container">
                 <div className="field-text">Provincia:</div>
                 <select id="citizenship">
-                <option value="france">Ile de France</option>
-                <option value="italy">Bretagne</option>
+                  <option value="france">Ile de France</option>
+                  <option value="italy">Bretagne</option>
                 </select>
               </div>
               <div className="field-container">
                 <div className="field-text">City:</div>
                 <select id="citizenship">
-                <option value="france">Paris</option>
-                <option value="italy">Marseille</option>
+                  <option value="france">Paris</option>
+                  <option value="italy">Marseille</option>
                 </select>
               </div>
               <div className="field-container">
