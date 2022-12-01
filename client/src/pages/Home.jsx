ae593ce0749063a19100277b7f32c7e7fc732d8a
Thomas Lopez <thomas1.lopez@epitech.eu>
2022-12-01 23:27:26 +0100

    [ADD] signup fields

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,15 +5,15 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-notifications/lib/notifications.css';
 
-import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 class Notifications extends React.Component {
   createNotification = (type) => {
     return () => {
       switch (type) {
         case 'info':
-            NotificationManager.info('Info message');
-            break;
+          NotificationManager.info('Info message');
+          break;
         case 'accept':
           NotificationManager.success('Info message');
           break;
@@ -29,7 +29,7 @@ function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -43,14 +43,24 @@ function getCookie(cname) {
 
 
 const Home = () => {
-  
-  
-  
+
+
+
   const [codiceFiscal, setCodiceFiscal] = useState("")
   const [password, setPassword] = useState("")
   const [registrationCodiceFiscal, setRegistrationCodiceFiscal] = useState("")
   const [registrationPassword, setRegistrationPassword] = useState("")
   const [registrationPasswordConfirmed, setRegistrationPasswordConfirmed] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [gender, setGender] = useState("")
+  const [dob, setDob] = useState("")
+  const [region, setRegion] = useState("")
+  const [country, setCountry] = useState("")
+  const [phone, setPhone] = useState("")
+
+
+
 
 
 
@@ -69,6 +79,27 @@ const Home = () => {
   const handleRegistrationPasswordConfirmedChange = (event) => {
     setRegistrationPasswordConfirmed(event.target.value)
   }
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
+  }
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value)
+  }
+  const handleSetGenderChange = (event) => {
+    setGender(event.target.value)
+  }
+  const handleDobChange = (event) => {
+    setGender(event.target.value)
+  }
+  const handleRegionChange = (event) => {
+    setRegion(event.target.value)
+  }
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value)
+  }
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value)
+  }
 
 
   const handleSubmission = (event) => {
@@ -82,7 +113,8 @@ const Home = () => {
     element.classList.add("visible");
     document.getElementById("login").classList.add("invisible")
   }
-  function checkRegister(codice,password,confirmedPassword) {
+
+  function checkRegister(codice, password, confirmedPassword) {
     if (codice.length === 6 && password === confirmedPassword && password.length != 0) {
       document.cookie = `codice=${codice}`
       document.cookie = `password=${password}`
@@ -98,6 +130,7 @@ const Home = () => {
       NotificationManager.error('All fields have to be filled');
     }
   }
+
   function checkLogin(codice, password) {
     if (getCookie("codice") === codice && getCookie("password") === password) {
       NotificationManager.success('Login success');
@@ -151,7 +184,7 @@ const Home = () => {
               </div>
               <div className="button-container">
                 <a className="role-button" onClick={() => {
-                  checkLogin(codiceFiscal,password)
+                  checkLogin(codiceFiscal, password)
                 }}>Connect</a>
               </div>
               <div className="text-container">
@@ -161,60 +194,152 @@ const Home = () => {
           </div>
           <div className="signup-container">
             <div className="signup-card" id="signup">
-            <div className="card-header">
-              REGISTER
-            </div>
-            <hr className="line" />
-            <form className="form-personal-data" onSubmit={console.log("submited")}>
-              <div className="field-container">
-                <div className="field-text">Codice Fiscal:</div>
-                <input
-                  className="field"
-                  id="outlined-basic"
-                  variant="outlined"
-                  fullWidth
-                  label="Search"
-                  onChange={handleRegistrationCodiceFiscalChange}
-                  value={registrationCodiceFiscal}
-                />
+              <div className="card-header">
+                REGISTER
               </div>
-              <div className="field-container">
-                <div className="field-text">Password:</div>
-                <input
-                  className="field"
-                  id="outlined-basic"
-                  variant="outlined"
-                  fullWidth
-                  label="Search"
-                  type="password"
-                  onChange={handleRegistrationPasswordChange}
-                  value={registrationPassword}
-                />
-              </div>
-              <div className="field-container">
-                <div className="field-text">Confirm Password:</div>
-                <input
-                  className="field"
-                  id="outlined-basic"
-                  variant="outlined"
-                  fullWidth
-                  type="password"
-                  label="Search"
-                  onChange={handleRegistrationPasswordConfirmedChange}
-                  value={registrationPasswordConfirmed}
-                />
-              </div>
-              <div className="button-container">
-                <a className="role-button" onClick={() => {
-                  checkRegister(registrationCodiceFiscal,registrationPassword,registrationPasswordConfirmed)
-              }}>Sign up</a>
-              </div>
+              <hr className="line" />
+              <form className="form-personal-data" onSubmit={console.log("submited")}>
+                <div className="field-container">
+                  <div className="field-text">First name:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleFirstNameChange}
+                    value={firstName}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Last name:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleLastNameChange}
+                    value={lastName}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Username:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleRegistrationCodiceFiscalChange}
+                    value={registrationCodiceFiscal}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Codice Fiscal:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleRegistrationCodiceFiscalChange}
+                    value={registrationCodiceFiscal}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Gender:</div>
+                  <select name="gender" id="gender-select">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Password:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    type="password"
+                    onChange={handleRegistrationPasswordChange}
+                    value={registrationPassword}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Confirm Password:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    type="password"
+                    label="Search"
+                    onChange={handleRegistrationPasswordConfirmedChange}
+                    value={registrationPasswordConfirmed}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Date of birth:</div>
+                  <input
+
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleDobChange}
+                    value={dob}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Region of birth:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleRegionChange}
+                    value={region}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Country:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handleCountryChange}
+                    value={country}
+                  />
+                </div>
+                <div className="field-container">
+                  <div className="field-text">Phone number:</div>
+                  <input
+                    className="field"
+                    id="outlined-basic"
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    onChange={handlePhoneChange}
+                    value={phone}
+                  />
+                </div>
+                <div className="button-container">
+                  <a className="role-button" onClick={() => {
+                    checkRegister(registrationCodiceFiscal, registrationPassword, registrationPasswordConfirmed)
+                  }}>Sign up</a>
+                </div>
               </form>
             </div>
           </div>
         </div>
       </div>
-      <NotificationContainer/>
+      <NotificationContainer />
     </div>
   );
 }
