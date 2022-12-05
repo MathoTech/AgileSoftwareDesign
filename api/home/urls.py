aca32df4250c6deea03bc8275d354896782fab61
Thomas Lopez <thomas1.lopez@epitech.eu>
2022-12-05 17:13:47 +0100

    [FIX] remove api/

--- api/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('student/', views.StudentList.as_view()),
-    path('student/<int:pk>/', views.StudentDetail.as_view()),
-    path('student-login/',views.student_login)
-]
